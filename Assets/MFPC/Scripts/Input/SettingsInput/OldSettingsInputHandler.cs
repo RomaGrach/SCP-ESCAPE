6180c7db484b59c98673614ff3a38dad42100adc
ulta3000 <154890042+ulta3000@users.noreply.github.com>
2025-03-02 19:14:31 +0300

    каво

---
+++ Assets/MFPC/Scripts/Input/SettingsInput/OldSettingsInputHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace MFPC.Input.SettingsInput
+{
+    public class OldSettingsInputHandler : MonoBehaviour, ISettingsInput
+    {
+        public event Action OnOpenSettings;
+
+        private GameObject _settingsField;
+
+        [SerializeField] // Отображаем в инспекторе, чтобы можно было назначить действие
+        private InputActionReference openSettingsAction;
+
+        private void Update()
+        {
+            // Проверяем, активировано ли действие (например, нажата клавиша Tab)
+            if (openSettingsAction != null && openSettingsAction.action.WasPressedThisFrame())
+            {
+                OnOpenSettings?.Invoke();
+
+                if (OnOpenSettings != null) UpdateUIInput();
+            }
+        }
+
+        public void SetSettingsField(GameObject settingsField)
+        {
+            _settingsField = settingsField;
+        }
+
+        public void UpdateUIInput()
+        {
+            Cursor.lockState = (_settingsField.activeSelf)
+                ? CursorLockMode.None
+                : CursorLockMode.Locked;
+        }
+    }
+}
