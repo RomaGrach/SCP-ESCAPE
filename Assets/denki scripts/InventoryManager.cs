f161c25c77573a885e801cf7dc982d187fd11d11
Salilonin <parshinakcyusha4@gmail.com>
2025-03-02 19:41:53 +0300

    1

--- Assets/denki scripts/InventoryManager.cs
+++ Assets/denki scripts/InventoryManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 public class InventoryManager : MonoBehaviour
 {
+<<<<<<< Updated upstream
     public GameObject inventoryPanel; // ������ ���������
     public Image[] itemSlots; // ������ ��� ���������
     private bool isInventoryOpen = false;
@@ -10,6 +11,36 @@ public class InventoryManager : MonoBehaviour
     {
         inventoryPanel.SetActive(false); // ��������� ����� ��� ������
     }
+=======
+    public static InventoryManager instance; // Singleton
+
+    public GameObject inventoryPanel; // Панель инвентаря
+    public Image[] itemSlots; // Ячейки-инвентаря
+    private InventoryItem[] inventory = new InventoryItem[10]; // 10 предметов
+    private bool isInventoryOpen = false;
+    private int selectedItemIndex = -1;
+
+    public Transform dropPoint; // Точка выброса предмета (назначь в Unity)
+    public GameObject itemPrefab; // Префаб выбрасываемого предмета
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        inventoryPanel.SetActive(false);
+    }
+
+>>>>>>> Stashed changes
     void Update()
     {
         // ��������/�������� ��������� �� ������� B
@@ -22,12 +53,46 @@ public class InventoryManager : MonoBehaviour
         // ������������ ��������� ��������� 1-5
         for (int i = 0; i < itemSlots.Length; i++)
         {
+<<<<<<< Updated upstream
             if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+=======
+            if ((i < 9 && Input.GetKeyDown(KeyCode.Alpha1 + i)) || (i == 9 && Input.GetKeyDown(KeyCode.Alpha0)))
+>>>>>>> Stashed changes
             {
-                SelectItem(i);
+                if (inventory[i] != null)
+                {
+                    SelectItem(i);
+                }
             }
         }
+<<<<<<< Updated upstream
     }
+=======
+
+        // Выбросить предмет при нажатии Q
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            DropItem();
+        }
+    }
+
+    public bool AddItem(InventoryItem newItem)
+    {
+        for (int i = 0; i < inventory.Length; i++)
+        {
+            if (inventory[i] == null)
+            {
+                inventory[i] = newItem;
+                itemSlots[i].sprite = newItem.icon;
+                itemSlots[i].enabled = true;
+                return true;
+            }
+        }
+        Debug.Log("Инвентарь полон!");
+        return false;
+    }
+
+>>>>>>> Stashed changes
     void SelectItem(int index)
     {
         selectedItemIndex = index;
@@ -39,5 +104,48 @@ public class InventoryManager : MonoBehaviour
             itemSlots[i].color = (i == index) ? Color.yellow : Color.white;
         }
     }
+<<<<<<< Updated upstream
 }
 
+=======
+
+    public void UseItem()
+    {
+        if (selectedItemIndex != -1 && inventory[selectedItemIndex] != null)
+        {
+            Debug.Log("Использован предмет: " + inventory[selectedItemIndex].itemName);
+            inventory[selectedItemIndex] = null;
+            itemSlots[selectedItemIndex].sprite = null;
+            itemSlots[selectedItemIndex].enabled = false;
+            selectedItemIndex = -1;
+        }
+    }
+
+    public void DropItem()
+    {
+        if (selectedItemIndex == -1 || inventory[selectedItemIndex] == null)
+        {
+            Debug.Log("Нет выбранного предмета для выброса!");
+            return;
+        }
+
+        Debug.Log("Выброшен предмет: " + inventory[selectedItemIndex].itemName);
+
+        // Определяем позицию выброса перед игроком
+        Transform playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        Vector3 dropPosition = playerTransform.position + playerTransform.forward * 1.5f; // 1.5 метра впереди
+
+        // Создаём выброшенный предмет перед игроком
+        GameObject droppedItem = Instantiate(itemPrefab, dropPosition, Quaternion.identity);
+        droppedItem.GetComponent<ItemPickup>().itemData = inventory[selectedItemIndex];
+
+        // Очищаем слот в инвентаре
+        inventory[selectedItemIndex] = null;
+        itemSlots[selectedItemIndex].sprite = null;
+        itemSlots[selectedItemIndex].enabled = false;
+
+        selectedItemIndex = -1;
+    }
+
+}
+>>>>>>> Stashed changes
